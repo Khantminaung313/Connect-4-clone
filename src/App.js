4171ff6beb31ddd17f0558742a28e1a7d64e441e
khantminaung313 <khantminaung313@gmail.com>
2024-04-19 10:47:34 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    };
-
-    fetchData();
-  });
-  return <></>;
+  return (
+    <>
+      <h1>Hello React World!</h1>
+    </>
+  );
 };
 
 export default App;
